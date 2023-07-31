164a2225f8ae5c0223350346e83e3ad0c98d5ddf
Henrique Caetano de Barros <henrique.barros@sc.senai.br>
2023-07-30 23:05:33 -0300

    Init Javascript

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 
-let numberOne = 0
-let numberTwo = 0
-let operator = ''
+let numberOne = '0'
+let numberTwo = '0'
+let operator = null
 
-function operate(operator, numberOne, numberTwo) {
-    
+
+function getNumber(number) {
+    if(operator == null) {
+        numberOne = numberOne == '0' ? number : numberOne + number
+    }
+    else {
+        numberTwo = numberTwo == '0' ? number : numberTwo + number
+    }
+}
+
+
+function getOperator(operatorParam) {
+    if(operator != null) {
+        operate()
+    }
+    else {
+        operator = operatorParam
+    }
+}
+
+
+function getDot() {
+    if(operator == null && !numberOne.includes(".")) {
+        numberOne += "."
+    }
+    else if(operator != null && !numberTwo.includes(".")) {
+        numberTwo += "."
+    }
+}
+
+
+function operate() {
+    if(operator == null) {
+        return
+    }
+    if(operator == '/' && parseFloat(numberOne) != 0 && parseFloat(numberTwo) == 0 ) {
+        alert('')
+        return
+    }
+    const calculation = document.getElementsByClassName("calculation")[0]
+    const response = document.getElementsByClassName("response")[0]
+
+    if(operator == '+') {
+        response.textContent = (parseFloat(numberOne) + parseFloat(numberTwo))
+        calculation.textContent = `${numberOne} + ${numberTwo} = ${response.textContent}` 
+    }
+    if(operator == '-') {
+        response.textContent = (parseFloat(numberOne) - parseFloat(numberTwo))
+        calculation.textContent = `${numberOne} - ${numberTwo} = ${response.textContent}` 
+    }
+    if(operator == '*') {
+        response.textContent = (parseFloat(numberOne) * parseFloat(numberTwo))
+        calculation.textContent = `${numberOne} x ${numberTwo} = ${response.textContent}` 
+    }
+    if(operator == '*') {
+        response.textContent = (parseFloat(numberOne) / parseFloat(numberTwo))
+        calculation.textContent = `${numberOne} / ${numberTwo} = ${response.textContent}` 
+    }
 }
